e797addee1c6232644f758846062ab8acb6f45b2
yukatou <yu.lets0226@gmail.com>
2017-01-28 15:39:36 +0900

    cache

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.gitconfig .gitignore .inputrc .vimrc .zshrc .zshenv .tmux.conf)
+DOT_FILES=(.gitconfig .gitignore .inputrc .vimrc .zshrc .zshenv .tmux.conf .cache)
 for file in ${DOT_FILES[@]}
 do
   echo "create symbolic link => $HOME/$file"
